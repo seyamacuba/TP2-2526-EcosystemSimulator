3e8cf8762fef457917f99a4e4bf5aeaededea836
Piccola Seyam Acuba <pacuba@ucm.es>
2026-02-16 23:23:52 +0100

    ahitamo

--- src/main/java/simulator/model/Wolf.java
+++ src/main/java/simulator/model/Wolf.java
@@ -1,6 +1,7 @@
 package simulator.model;
 
 import simulator.misc.Vector2D;
+import simulator.misc.Utils;
 
 public class Wolf extends Animal {
   final static String WOLF_GENETIC_CODE = "Wolf";
@@ -17,13 +18,23 @@ public class Wolf extends Animal {
   final static double DESIRE_INCREASE_RATE_WOLF = 30.0;
   final static double PREGNANT_PROBABILITY_WOLF = 0.75;
 
+  private SelectionStrategy huntingStrategy;
+  private Animal huntTarget;
+
   protected Wolf(SelectionStrategy mateStrategy, SelectionStrategy huntingStrategy, Vector2D pos) {
     super(WOLF_GENETIC_CODE, Diet.CARNIVORE, INIT_SIGHT_WOLF, INIT_SPEED_WOLF, mateStrategy, pos);
-    //Terminar Constructor
+    this.huntingStrategy = huntingStrategy;
+    this.huntTarget = null;
   }
 
   protected Wolf(Wolf p1, Animal p2) {
     super(p1, p2);
+    this.huntingStrategy = p1.huntingStrategy;
+    this.huntTarget = null;
+  }
+
+  private double limitar(double val, double min, double max){
+    return Math.max(min, Math.min(max, val));
   }
 
   @Override
@@ -41,33 +52,140 @@ public class Wolf extends Animal {
     return getBaby() != null;
   }
 
+  //Lista de updates, aunque yo lo pondria en animal, porque sheep tmb va a usar esto.
+private void updateNormal(double dt){
+  Vector2D distanciaVector = this.getDestination().minus(this.getPos()); //Obtengo la diferencia
+  double distancia = distanciaVector.magnitude();
+
+  if(distancia < 8.0){
+    this.setDest(Vector2D.get_random_vector(0,getRegionMngr().getWidth())); //ns yo esto
+  }
+  double velocidad = INIT_SPEED_WOLF*dt*Math.exp((getEnergy()-100.0) * 0.007);
+  move(velocidad);
+
+  this.setAge(this.getAge() + dt);
+  this.setEnergy(limitar(this.getEnergy() - 18.0 * dt, 0.0, 100.0));
+  this.setDesire(limitar(getDesire() + 30.0 * dt, 0.0, 100.0));
+
+  if(getEnergy() < 50.0){
+    this.setState(State.HUNGER);
+  }else if(getDesire() > 65.0){
+    this.setState(State.MATE);
+  }
+}
+
+private void updateHunger(double dt){
+    if(this.huntTarget != null && this.getState() == State.DEAD || getMateTarget().getPos().minus(getPos()).magnitude() > getSightRange()){
+      setMateTarget(null);
+    }
+
+    //Si esta null.
+  if(this.huntTarget == null){
+    double velocidad = INIT_SPEED_WOLF*dt*Math.exp((getEnergy()-100.0) * 0.007);
+    move(velocidad); //Avanzo
+  }else{
+    this.setDest(huntTarget.getPos());
+    setAge(getAge()+dt);
+    double velocidad = 3.0*INIT_SPEED_WOLF*dt*Math.exp((getEnergy()-100.0) * 0.007);
+    move(velocidad); //Avanzo
+    this.setEnergy(limitar(this.getEnergy() - 18.0 * dt, 0.0, 100.0));
+    this.setDesire(limitar(getDesire() + 30.0 * dt, 0.0, 100.0));
+
+    Vector2D VectorDistancia = this.huntTarget.getPos().minus(this.getPos());
+    double distancia = VectorDistancia.magnitude(); //OBTENGO LA DISTANCIA.
+
+    if(distancia < 8.0){
+      huntTarget.setState(State.DEAD);
+      this.huntTarget = null;
+      setEnergy(limitar(getEnergy() + 50.0, 0.0, 100.0));
+    }
+
+    if(getEnergy() > 50.0){
+      if(getEnergy() < 65.0){
+        setState(State.NORMAL);
+      }else{
+        setState(State.MATE);
+      }
+    }
+
+  }
+}
+
+private void updateMate(double dt){
+
+}
+
+//MÉTODO UPDATE GENERAL:
   @Override
   public void update(double dt) {
+    if(this.getState() == State.DEAD) return;
+    switch(getState()){
+      case NORMAL -> {
+        updateNormal(dt);
+      }
+      case MATE -> {
+        updateMate(dt);
+      }
+      case HUNGER -> {
+        updateHunger(dt);
+      }
+      case DANGER -> {
+        //NO PUEDE ESTAR NUNCA EN DANGER.
+      }
+      case DEAD -> {
+      }
 
+    }
+
+    //SI esta fuera del mapa, ajusta y cambia a normal.
+    if(getPos().getX() >= getRegionMngr().getWidth() || getPos().getX() < 0 ||
+      getPos().getY() >= getRegionMngr().getHeight() || getPos().getY() < 0){
+
+      double xAjustada = limitar(getPos().getX(), 0, getRegionMngr().getWidth()-1);
+      double yAjustada = limitar(getPos().getY(), 0, getRegionMngr().getHeight()-1);
+
+      Vector2D posicionNueva = new Vector2D(xAjustada,yAjustada);
+      this.setPos(posicionNueva);
+
+      setState(State.NORMAL);
+    }
+
+    //Si ya no tiene ganas de vivir o ya esta fosil, apaga y vamonos
+    if(getEnergy() <= 0.0 || getAge() > MAX_AGE_WOLF){
+      setState(State.DEAD);
+    }
+
+    //Si esta fallecido, apaga y vamonos
+    if(getState() != State.DEAD){
+      double food = getRegionMngr().getFood(this,dt);
+      setEnergy(limitar(this.getEnergy() + food, 0.0, 100.0));
+    }
   }
 
   @Override
   protected void setNormalStateAction() {
-
+    this.huntTarget = null;
+    this.setMateTarget(null);
   }
 
   @Override
   protected void setHungerStateAction() {
-
+    this.setMateTarget(null);
   }
 
   @Override
   protected void setDeadStateAction() {
-
+    this.huntTarget = null;
+    this.setMateTarget(null);
   }
 
   @Override
   protected void setDangerStateAction() {
-
+//Nunca puede estar en DANGER.
   }
 
   @Override
   protected void setMateStateAction() {
-
+    huntTarget = null;
   }
 }
